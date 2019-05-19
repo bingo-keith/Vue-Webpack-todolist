99baf1f229d285fee3971d369196d8256f90dd44
hg.wb.zheng <hg.wb.zheng@gmail.com>
2019-05-19 22:21:38 +0800

    add webpack style-loader css-loader url-loader and file-loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './app.vue'
+import './assets/images/money.jpg'
+import './assets/styles/test.css'
 
-const root = document.createElement('div')
-document.body.appendChild(root)
+const root = document.createElement('div');
+document.body.appendChild(root);
 
 new Vue({
   render: (h) => h(App)
-}).$mount(root)
+}).$mount(root);
