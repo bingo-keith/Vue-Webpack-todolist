0bc0c596f5b46c26555c1fe6d7a877dbd77b3d8d
hg.wb.zheng <hg.wb.zheng@gmail.com>
2019-05-22 21:15:50 +0800

    resolve an error such as Module not found: Error: Can't resolve './todo/header'

--- src/app.vue
+++ src/app.vue
@@ -1,10 +1,36 @@
 <template>
+  <div id="app">
+    <div id="cover">
+      <Header></Header>
+    </div>
+  </div>
 </template>
 
 <script>
+  import Header from './todo/header.vue'
   export default {
+    components: {
+      Header,
+    }
   }
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
+  #app {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+  }
+  #cover {
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    background-color #999
+    opacity .9
+    z-index -1
+  }
 </style>
